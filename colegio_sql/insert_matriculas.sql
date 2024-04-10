3adfd09dd34ef7cb02d73735090687c57677a890
Hadson00 <hadson798@gmail.com>
2024-04-09 21:53:07 -0300

    matricula

---
+++ colegio_sql/insert_matriculas.sql
@@ -0,0 +1,22 @@
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(2,1,233.50,10.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(4,3,233.50,30.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(3,5,233.50,20.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(1,4,233.50,15.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(4,2,233.50,30.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(3,6,233.50,40.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(2,10,233.50,20.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(1,8,233.50,10.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(4,7,233.50,15.00,'2013-01-01','2013-12-20');
+INSERT INTO `matriculas`(`turmas_idturmas`,`alunos_idalunos`,`mensalidade`,`desconto`,`dtinicio`,`dtfim`)
+VALUES(4,9,233.50,25.00,'2013-01-01','2013-12-20');
+
+SELECT * FROM `matriculas`
