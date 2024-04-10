dd55c973adaa67ae091bda408118c25a9a176da7
RaphaZY <raphacoelho3790@gmail.com>
2024-04-09 21:38:41 -0300

    grade

---
+++ colegio_sql/insert-Grade.sql
@@ -0,0 +1,39 @@
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (1,1);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (2,1);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (3,1);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (4,1);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (1,2);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (2,2);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (3,2);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (4,2);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (2,3);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (3,3);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (4,3);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (2,4);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (4,4);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (1,5);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (2,5);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (1,6);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (2,6);
+INSERT INTO `grade` (`turmas_idturmas`,`materias_idmaterias`)
+VALUES (3,6);
+
+SELECT * FROM `grade`
+	ORDER BY `materias_idmaterias`
