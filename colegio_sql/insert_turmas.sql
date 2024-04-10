85c737406aaf9dbf19e3b7d6e146a449350032c1
Hadson00 <hadson798@gmail.com>
2024-04-09 21:02:19 -0300

    InsertTurmas

---
+++ colegio_sql/insert_turmas.sql
@@ -0,0 +1,10 @@
+INSERT INTO `turmas` (`turma`)
+VALUES('6 ano');
+INSERT INTO `turmas` (`turma`)
+VALUES('7 ano');
+INSERT INTO `turmas` (`turma`)
+VALUES('8 ano');
+INSERT INTO `turmas` (`turma`)
+VALUES('9 ano');
+
+SELECT * FROM `turmas`
